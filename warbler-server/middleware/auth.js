ab5ac87b46a86dcbd62bb36a82b578c5ef1dc395
Samanthi Jinadasa <baghya.samanthi@gmail.com>
2021-03-08 14:58:39 +0000

    auth.js in middleware folder

---
+++ warbler-server/middleware/auth.js
@@ -0,0 +1,24 @@
+require("dotenv").load();
+// bring in environment variables
+
+
+// bring in JSON web token module
+const jwt = require("jsonwebtoken");
+
+// make sure the user is logged in
+// Authentication
+// this is the idea of Authentication
+exports.loginRequired = function(req, res, next) {
+    
+    
+};
+
+
+// make sure we get the correct user
+// Authorization
+// now are you actually allowed to do this
+
+
+
+
+
